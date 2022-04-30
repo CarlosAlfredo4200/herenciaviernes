45854791f47736ed67db63a076394494eaa2447d
Carlos <alfredomontoyagit@gmail.com>
2022-04-29 20:41:42 -0500

    Herencia

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #Herencia en python
+from classes.Cliente import Cliente
+
+
+#Crear un obj de la clase hija
+
+hijo = Cliente()
+
+hijo.nombre = "Carlos Montoya"
+hijo.saldo = 5000
+hijo.saludar()
